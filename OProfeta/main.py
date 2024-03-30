e040c80fb5ad8082238d5cd93e32deb6d673e284
LucasRodrigues2000 <lucasrodriguespuc@gmail.com>
2024-03-30 00:29:20 -0300

    pega ai hugo

--- OProfeta/main.py
+++ OProfeta/main.py
@@ -5,5 +5,12 @@ import yfinance as YahooFinance
 #criando banco de dados.
 conn = sqlite3.connect('stocks.db')
 
-#Download Data
+url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
+# Leitura da tabela HTML como DataFrame
+df = pd.read_html(url, index_col=0)[0]
+
+# Exibição do DataFrame
+print(df.to_string())
+
+#Download Data
